ffff3ac6cb874302bd160da9727e80a16a407506
luoyuntian <yuntian4@illinois.edu>
2018-09-12 16:52:23 -0500

    new

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,21 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                if (maze.canMove() == true) {
+                    maze.move();
+                    maze.turnRight();
+                    if (maze.canMove() == false) {
+                        maze.turnLeft();
+                    }
+                } else {
+                    maze.turnLeft();
+                    if (maze.canMove() == false) {
+                        maze.turnRight();
+                        maze.turnRight();
+                    }
+                }
+            }
         }
 
         if (maze.isFinished()) {
